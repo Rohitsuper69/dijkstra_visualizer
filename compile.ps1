de602f0b01cf38cd2edef2276c05db85cabeb5bc
Rohitsuper69 <rohitu.chakraborty.che21@itbhu.ac.in>
2024-09-05 11:33:53 +0530

    first commit

---
+++ compile.ps1
@@ -0,0 +1,2 @@
+g++ -c main.cpp -I"C:\Users\rohit56\Documents\libraries\SFML-2.6.1-windows-gcc-13.1.0-mingw-64-bit\SFML-2.6.1\include"
+g++ main.o -o main -L"C:\Users\rohit56\Documents\libraries\SFML-2.6.1-windows-gcc-13.1.0-mingw-64-bit\SFML-2.6.1\lib" -lsfml-graphics -lsfml-window -lsfml-system
